196f7cb13ab57056159f30e2fe494613e85117cf
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-15 11:19:42 +0900

    DAO 검색키 조회, 기간별조회 추가

--- java/member/database/MemberDAO.java
+++ java/member/database/MemberDAO.java
@@ -18,16 +18,15 @@ public class MemberDAO {
 	private MemberVO vo = null;
 	private String sql = new String("");
 
-	//페이징 총 레코드건수//관리자의 경우 level=0, 관리자가 아닐 경우 level=99
-	public int totRecCnt(int level) {
+	//전체회원목록 페이징(총 레코드건수) - 운영자회원(level=4) : userDel='NO'(활동중), userInfo='공개' 회원조회
+	public int memberListTotRecCnt(char kindYmd, int term) {
 		int totRecCnt = 0;
 		try {
-			if (0 == level) { //관리자
-				sql = "select count(*) as totRecCnt from member";
-			} else { //운영자회원
-				sql = "select count(*) as totRecCnt from member where userInfo = '공개' ";
-			}
+			String addPrepareSQL1 = makeIntervalSQL(kindYmd, term, "startDate");
+			sql = "select count(*) as totRecCnt from member where userDel = 'NO' and userInfo = '공개' ";
+			if(0 < addPrepareSQL1.length()) sql = sql + "and " + addPrepareSQL1;
 			pstmt = conn.prepareStatement(sql);
+			if(0 < addPrepareSQL1.length()) pstmt.setInt(1, term);
 			rs = pstmt.executeQuery();
 			rs.next(); //ResultSet레코드움직이기(count함수는 무조건 0값조차 가져옴)
 			totRecCnt = rs.getInt("totRecCnt");
@@ -39,44 +38,40 @@ public class MemberDAO {
 		}
 		return totRecCnt;
 	}
-	
-	//기긴별 조회 SQL 조건문 추가 - Interval 
-	private String makeIntervalSQL(char kindYmd, int recently, String columnName, boolean addWhere) {
+	//기간별 조회 SQL 조건문 추가 - Interval ex) interval 5 day, interval 1 Month
+	private String makeIntervalSQL(char kindYmd, int term, String columnName) {
 		String sqlInterval = null;
-		if ((0 < kindYmd 
+		if ((0 != kindYmd 
 			&& ('Y' == kindYmd || 'M' == kindYmd || 'D' == kindYmd)) 
-			&& 0 < recently) {
+			&& 0 < term) {
 			String sqlIntervalDate = new String("interval ? ");
 			switch(kindYmd) {
 				case 'Y' : sqlIntervalDate += "year"; break;
 				case 'M' : sqlIntervalDate += "month"; break;
+				case 'W' : sqlIntervalDate += "week"; break;
 				case 'D' : sqlIntervalDate += "day"; break;
 				default : break;
 			}
 			String sqlTerm = new String("date_sub(now(), " + sqlIntervalDate + ")");
-			if (addWhere)
-				sqlInterval = new String("Where " + sqlTerm + " <= " + columnName + " and " + columnName +" <= now() ");
-			else
-				sqlInterval = new String(sqlTerm + " <= " + columnName + " and " + columnName +" <= now() ");
+			sqlInterval = new String(sqlTerm + " <= " + columnName + " and " + columnName +" <= now() ");
 		} else {
 			sqlInterval = new String("");
 		}
 		return sqlInterval;
 	}
-	//관리자의 경우 level=0, 관리자가 아닐 경우 level=99
-	public List<MemberVO> searchMemberList(int level, int startIndexNo, int pageSize) {
+	//전체회원목록 - 운영자회원(level=4) : userDel='NO'(활동중), userInfo='공개' 회원조회
+	public List<MemberVO> searchMemberList(char kindYmd, int term, int startIndexNo, int pageSize) {
 		List<MemberVO> vos = new ArrayList<>();
 		try {
 			int prepareIdx = 0;
-			String addPrepareSQL1 = makeIntervalSQL(kindYmd, recently, "vDate", true);
-			if (0 == level) { //관리자
-				sql = "select *, timestampdiff(day, lastDate, now()) as overDaysUserDel from member order by idx desc limit ?, ? ";
-			} else {
-				sql = "select * from member where userInfo = '공개' order by idx desc limit ?, ? ";
-			}
+			String addPrepareSQL1 = makeIntervalSQL(kindYmd, term, "startDate");
+			sql = "select * from member where userDel = 'NO' and userInfo = '공개' ";
+			if(0 < addPrepareSQL1.length()) sql = sql + addPrepareSQL1;
+			sql += "order by idx desc limit ?, ? ";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, startIndexNo);
-			pstmt.setInt(2, pageSize);
+			if(0 < addPrepareSQL1.length()) pstmt.setInt(++prepareIdx, term);
+			pstmt.setInt(++prepareIdx, startIndexNo);
+			pstmt.setInt(++prepareIdx, pageSize);
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
 				vo = new MemberVO();
@@ -103,9 +98,7 @@ public class MemberDAO {
 				vo.setStartDate(rs.getString("startDate"));
 				vo.setLastDate(rs.getString("lastDate"));
 				vo.setTodayCnt(rs.getInt("todayCnt"));
-				if (0 == level) { //관리자
-					vo.setOverDaysUserDel(rs.getInt("overDaysUserDel"));
-				}
+				vo.setOverDaysUserDel(rs.getInt("overDaysUserDel"));
 				vos.add(vo);
 			}
 		} catch (SQLException e) {
@@ -126,8 +119,8 @@ public class MemberDAO {
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				vo = new MemberVO();
-				vo.setIdx(rs.getInt("idx"));//pk(idx,mid)
-				vo.setMid(rs.getString("mid"));//pk(idx,mid)
+				vo.setIdx(rs.getInt("idx"));
+				vo.setMid(rs.getString("mid"));
 				vo.setNickName(rs.getString("nickName"));
 				vo.setLevel(rs.getInt("level"));
 				vo.setLastDate(rs.getString("lastDate"));
@@ -147,7 +140,7 @@ public class MemberDAO {
 	public String searchMid(String email, String pwd) {
 		String mid = null;
 		try {
-			sql = "select mid from member where email = ? and pwd = ? ";
+			sql = "select mid from member where userDel = 'No' and email = ? and pwd = ? ";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, email);
 			pstmt.setString(2, new SecurityUtil().encryptSHA256(pwd));
@@ -166,11 +159,11 @@ public class MemberDAO {
 	public int updatePwd(String mid, String email, String pwd) {
 		int res = 0;
 		try {
-			sql = "update member set pwd = ? where mid = ? and email = ? ";
+			sql = "update member set pwd = ? where userDel = 'No' and mid = ? and email = ? ";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1 , mid);
-			pstmt.setString(2 , email);
-			pstmt.setString(3 , new SecurityUtil().encryptSHA256(pwd));
+			pstmt.setString(1 , new SecurityUtil().encryptSHA256(pwd));
+			pstmt.setString(2 , mid);
+			pstmt.setString(3 , email);
 			res = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("SQL 에러 : " + e.getMessage());
@@ -181,11 +174,12 @@ public class MemberDAO {
 	}
 
 	//회원 개별정보 취득
-	public MemberVO search(String sMid) {
+	public MemberVO search(int idx, String mid) {
 		try {
-			sql = "select * from member where mid = ? and userDel = 'No'";
+			sql = "select * from member where idx = ? and mid = ? and userDel = 'No'";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, sMid);
+			pstmt.setInt(1, idx);
+			pstmt.setString(2, mid);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				vo = new MemberVO();
@@ -308,94 +302,14 @@ public class MemberDAO {
 		return res;
 	}
 
-	//오늘 처음 방문시 오늘방문카운트(todayCnt)를 0으로 셋팅한다.
-	public int updateTodayCnt(String mid) {
-		int res = 0;
-		try {
-			sql = "update member set todayCnt = 0 where mid = ? ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1 , mid);
-			res = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQL 에러 : " + e.getMessage());
-		} finally {
-			instance.pstmtClose();
-		}
-		return res;
-	}
-	
-	//방문수, 오늘방문수, 포인트 증가
-	public int updateVisitCntAndTodayCntAndPoint(String mid) {
-		int res = 0;
-		try {
-			sql = "update member set point = point+1, visitCnt = visitCnt+1, todayCnt = todayCnt+1, lastDate=now() where mid = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1 , mid);
-			res = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQL 에러 : " + e.getMessage());
-		} finally {
-			instance.pstmtClose();
-		}
-		return res;
-	}
-
-	//회원탈퇴 - 1달간은 회원정보유지, userDel만 OK(회원탈퇴)로 수정
-	public int updateUserDel(String mid) {
-		int res = 0;
-		try {
-			sql = "update member set userDel = 'OK' where mid = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1 , mid);
-			res = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQL 에러 : " + e.getMessage());
-		} finally {
-			instance.pstmtClose();
-		}
-		return res;	
-	}
-	
-	//관리자 -전체회원 목록(공개회원)->선택된 회원의 등급 수정
-	public int updateMemberLevel(String level, String mid) {
-		int res = 0;
-		try {
-			sql = "update member set level = ? and mid = ? ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, Integer.parseInt(level));
-			pstmt.setString(2, mid);
-			res = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQL 에러 : " + e.getMessage());
-		} finally {
-			instance.pstmtClose();
-		}
-		return res;	
-	}
-
-	//회원탈퇴신청(userDel) 후 30일 경과시, 회원삭제 처리
-	public int delete(String mid) {
-		int res = 0;
-		try {
-			sql = "delete from member where mid = ? and 30 <= timestampdiff(day, lastDate, now()) and userDel = 'OK'";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, mid);
-			res = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQL 에러 : " + e.getMessage());
-		} finally {
-			instance.pstmtClose();
-		}
-		return res;	
-	}
-
+	//개별회원정보수정
 	public int update(MemberVO vo) {
 		int res = 0;
 		try {
 			sql = "update member set"
 					+ "		nickName = ?, name = ?, gender = ?, birthday = ?, tel = ?, address = ?,"
 					+ "		email = ?, homepage = ?, job = ?, hobby = ?, photo = ?, content = ?, userInfo = ? "
-					+ "where mid = ? ";
+					+ "where idx = ? and mid = ? and userDel = 'No' ";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1 , vo.getNickName());
 			pstmt.setString(2 , vo.getName());
@@ -410,7 +324,8 @@ public class MemberDAO {
 			pstmt.setString(11 , vo.getPhoto());
 			pstmt.setString(12 , vo.getContent());
 			pstmt.setString(13 , vo.getUserInfo());
-			pstmt.setString(14 , vo.getMid());
+			pstmt.setInt(14 , vo.getIdx());
+			pstmt.setString(15 , vo.getMid());
 			res = pstmt.executeUpdate();
 
 		} catch (SQLException e) {
@@ -420,5 +335,55 @@ public class MemberDAO {
 		}
 		return res;
 	}
+	
+	//오늘방문카운트(todayCnt) - 초기값 : 0
+	public int updateTodayCnt(int idx, String mid) {
+		int res = 0;
+		try {
+			sql = "update member set todayCnt = 0 where idx = ? and mid = ? and userDel = 'No' ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1 , idx);
+			pstmt.setString(2 , mid);
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("SQL 에러 : " + e.getMessage());
+		} finally {
+			instance.pstmtClose();
+		}
+		return res;
+	}
+	
+	//방문수, 오늘방문수, 포인트 증가
+	public int updateVisitCntAndTodayCntAndPoint(int idx, String mid) {
+		int res = 0;
+		try {
+			sql = "update member set point = point+1, visitCnt = visitCnt+1, todayCnt = todayCnt+1, lastDate=now() where idx = ? and mid = ? and userDel = 'No' ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1 , idx);
+			pstmt.setString(2 , mid);
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("SQL 에러 : " + e.getMessage());
+		} finally {
+			instance.pstmtClose();
+		}
+		return res;
+	}
 
+	//개별회원탈퇴 - 30일 회원정보유지(userDel=OK(회원탈퇴), lastDate=오늘(탈퇴날짜) 수정)
+	public int updateUserDel(int idx, String mid) {
+		int res = 0;
+		try {
+			sql = "update member set userDel = 'OK', lastDate = now() where idx = ? and mid = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1 , idx);
+			pstmt.setString(2 , mid);
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("SQL 에러 : " + e.getMessage());
+		} finally {
+			instance.pstmtClose();
+		}
+		return res;	
+	}
 }
