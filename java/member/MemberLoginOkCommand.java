32f611c5a553902c9357cd1bb84fe06d8b6990ff
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-15 20:31:11 +0900

    회원로그인->회원전용방 설정값 셋팅

--- java/member/MemberLoginOkCommand.java
+++ java/member/MemberLoginOkCommand.java
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import conn.SecurityUtil;
+import board.database.BoardDAO;
+import guest.database.GuestDAO;
 import member.database.MemberDAO;
 import member.database.MemberVO;
 
@@ -18,54 +19,79 @@ public class MemberLoginOkCommand implements MemberInterface {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String mid = request.getParameter("mid");
-		String pwd = request.getParameter("pwd");
-		String idSave = request.getParameter("idSave");
-		
-		//비밀번호 암호화처리-SHA2(sha256)
-		String shaPwd = new SecurityUtil().encryptSHA256(pwd);
+		String mid = request.getParameter("mid");//login요청-mid
+		String pwd = request.getParameter("pwd");//login요청-pwd
+		String idSave = request.getParameter("idSave");//logoin요청-idSave저장(mid쿠키저장)
 
+		HttpSession session = request.getSession();
+		String sPoint = (String)session.getAttribute("sPoint");
+		int intSPoint = 0;//int타입의 포인트(세션정보)
+		if (null != sPoint && 0 < sPoint.trim().length()) intSPoint = Integer.parseInt(sPoint);
+			
+		// --------------------------------------------------
+		// 로그인 성공시 처리 내용
+		// --------------------------------------------------
+		// 1.오늘방문횟수, 전체방문횟수 1씩 증가 
+		// 2.포인터 1씩 증가(방문시마다 1포인트씩 증가하며 1일 10회 이하, 가입시 100 포인트 증가-insert시 db default 증가)
+		// 3.주요자료 세션 저장 
+		// 4.쿠키에 아이디저장유무? 
+		// --------------------------------------------------
 		MemberDAO dao = new MemberDAO();
-		MemberVO vo = dao.searchMemberLogin(mid, shaPwd);
+		int res = 0;
+		MemberVO vo = dao.searchMemberLogin(mid, pwd);
+		//DB nickName은 not null이며, 닉네임 세션 저장하여 전체 웹페이지마다 닉네임 출력 처리
 		if (null == vo || 0 == vo.getNickName().trim().length()) {
 			request.setAttribute("msg", "memberLoginNo");
 			request.setAttribute("url", request.getContextPath() + "/memberLogin.mbr");
+			return;
 		}
-		
-		//로그인 성공 (1.주요자료 세션 저장, 2. 오늘 방문횟수, 3.방문수,포인터 증가, 4.쿠키에 아이디저장유무? 
-		HttpSession session = request.getSession();
 		session.setAttribute("sMid", vo.getMid());
-		session.setAttribute("sNickName", vo.getNickName());
 		session.setAttribute("sLevel", vo.getLevel());
-//		session.setAttribute("sLastDate", vo.getLastDate());
-//		session.setAttribute("sPoint", vo.getPoint());
-//		session.setAttribute("sTodayCnt", vo.getTodayCnt());
-//		session.setAttribute("sVisitCnt", vo.getVisitCnt());
+		session.setAttribute("sLevelName", vo.getLevelName());
+		session.setAttribute("sNickName", vo.getNickName());
+		session.setAttribute("sLastDate", vo.getLastDate());
 		
-    String strNow = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-
-    System.out.println("vo:" + vo);
-		System.out.println("strNow : "+strNow);
-		
+		// --------------------------------------------------
+		// DB 저장 : 오늘방문횟수, 전체방문횟수, 포인터 1씩 증가
+		// --------------------------------------------------
 		//최종방문일과 오늘날짜 비교해서 다른 경우, 오늘방문횟수(todayCnt)값을 0으로 초기화
-		if (vo.getLastDate().substring(0, 10).equals(strNow)) {
-			int res = dao.updateTodayCnt(mid);
-		}
-
-//학생질문 - ?
-//		int point = vo.getPoint();
-//		if (vo.getTodayCnt() > 10) point++;
+    String strNow = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+		if (! vo.getLastDate().substring(0, 10).equals(strNow)) 
+			dao.updateTodayCnt(vo.getIdx(), vo.getMid());//todayCnt초기화 : 0
+		//1.오늘방문횟수, 전체방문횟수 1씩 증가 
+		dao.updateVisitCntAndTodayCnt(vo.getIdx(), vo.getMid());
+		//2.포인터 1씩 증가(방문시마다 1포인트씩 증가<세션저장,DB저장>, 1일 10회 이하)
+		if (0 <= intSPoint && 10 >= intSPoint) //포인트 1일 10회 이하(세션정보값으로 체크)
+			dao.updatePoint(vo.getIdx(), vo.getMid());//DB포인트 1포인트 증가
 		
-		int res = dao.updateVisitCntAndTodayCntAndPoint(mid);
+		// --------------------------------------------------
+		// 세션 저장 : 오늘방문횟수, 전체방문횟수, 포인터 1씩 증가
+		// --------------------------------------------------
+		session.setAttribute("sTodayVCnt", vo.getTodayCnt() + 1);
+		session.setAttribute("sVCnt", vo.getVisitCnt() + 1);
+		session.setAttribute("sPoint", ++intSPoint);
 		
-		//쿠키에 아이디(mid)를 저장 checkbox checked 클릭 여부 - on/null
+		// --------------------------------------------------
+		// 세션 저장 : 활동내역(방명록에 올린 글 수, 게시판에 올린 글 수, 자료실에 올린 글 수
+		// --------------------------------------------------
+		BoardDAO boardDao = new BoardDAO();
+		GuestDAO guestDao = new GuestDAO();
+		session.setAttribute("sGuWritingCnt", 
+				guestDao.searchGuestWriteCnt(vo.getMid(), vo.getName()));
+		session.setAttribute("sBdWritingCnt", 
+				boardDao.searchBoardWriteCnt(vo.getMid(), vo.getNickName())
+				+ boardDao.searchBoardreplyWriteCnt(vo.getMid(), vo.getNickName()));
+		//자료실은 수업 아직 안했음
+		//session.setAttribute("sPdsWritingCnt", 
+		//	new PdsDAO().searchPdsWriteCnt(vo.getMid(), vo.getName(), vo.getNickName()));
+		
+System.out.println("idSave= " + idSave);		
+		//idSave체크시 : 쿠키에 아이디(mid)를 저장 checkbox checked 클릭 여부 - on/null
 		Cookie cookie = new Cookie("cMid", mid);
-		if (null != idSave && idSave.equals("on")) {
+		if (null != idSave && idSave.equals("on")) 
 			cookie.setMaxAge(60*60*24*7); //쿠키 만료시간은 7일
-		} 
-		else {
+		else 
 			cookie.setMaxAge(0); //쿠키 삭제
-		}
 		response.addCookie(cookie);
 
 		request.setAttribute("msg", "memberLoginOk");
